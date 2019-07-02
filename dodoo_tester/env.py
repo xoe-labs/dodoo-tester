8fcaadfb264f7fe09e5bf01553cafd4418b5744c
David Arnold <dar@xoe.solutions>
2019-07-02 11:02:50 +0200

    [IMP] add test run counter

--- dodoo_tester/env.py
+++ dodoo_tester/env.py
@@ -9,28 +9,54 @@ from .format import process
 
 @contextmanager
 def OdooTestExecution(self):
+    connection = {"dbname": self.database}
+    if odoo.tools.config["db_host"]:
+        connection.update({"host": odoo.tools.config["db_host"]})
+    if odoo.tools.config["db_port"]:
+        connection.update({"port": odoo.tools.config["db_port"]})
+    if odoo.tools.config["db_user"]:
+        connection.update({"user": odoo.tools.config["db_user"]})
+    if odoo.tools.config["db_password"]:
+        connection.update({"password": odoo.tools.config["db_password"]})
+    with closing(psycopg2.connect(**connection)) as conn, conn.cursor() as cr:
+        cr.execute(
+            """ALTER TABLE ir_logging ADD COLUMN IF NOT EXISTS "test_run" INTEGER;"""
+        )
+        conn.commit()
     try:
         yield odoo.service.server.start(preload=[self.database], stop=True)
     finally:
-        connection = {"dbname": self.database}
-        if odoo.tools.config["db_host"]:
-            connection.update({"host": odoo.tools.config["db_host"]})
-        if odoo.tools.config["db_port"]:
-            connection.update({"port": odoo.tools.config["db_port"]})
-        if odoo.tools.config["db_user"]:
-            connection.update({"user": odoo.tools.config["db_user"]})
-        if odoo.tools.config["db_password"]:
-            connection.update({"password": odoo.tools.config["db_password"]})
         with closing(psycopg2.connect(**connection)) as conn, conn.cursor() as cr:
             cr.execute(
-                """SELECT name, level, message, path, func, line
-                          FROM ir_logging"""
+                """
+                SELECT name,
+                       LEVEL,
+                       message,
+                       path,
+                       func,
+                       line
+                FROM ir_logging WHERE test_run IS NULL;
+                """
             )
             logs = cr.fetchall()
+            # Mark logs for subsequent runs to filter.
+            cr.execute(
+                """
+                SELECT DISTINCT test_run
+                FROM ir_logging
+                WHERE test_run IS NOT NULL;
+
+                """
+            )
+            next_run = max([run for (run,) in cr.fetchall()] or [0]) + 1
+            cr.execute(
+                """UPDATE ir_logging SET test_run = %s WHERE test_run IS NULL;""",
+                (next_run,),
+            )
             # PostgreSQL 9.2 renamed pg_stat_activity.procpid to pid:
             # http://www.postgresql.org/docs/9.2/static/release-9-2.html#AEN110389
             pid_col = "pid" if conn.server_version >= 90200 else "procpid"
-
+            conn.commit()
             cr.execute(
                 """
                 SELECT pg_terminate_backend(%(pid_col)s)
